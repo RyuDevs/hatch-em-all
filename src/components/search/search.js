d423da99b2c3d7df06bde5d75c88489983a6b6c2
Ryu Devs <ryu.devcreations@gmail.com>
2022-11-07 16:08:25 +0100

    Fix overflow pokemon scrolling

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,22 +2,21 @@ import React, { useEffect, useState } from "react";
 import SearchResults from "../searchResults/searchResults";
 import './search.css';
 
-const Search = ({pokemonCache, speciesCache, language, handleSpeciesCache, loadSpecies}) => {
+const Search = ({ pokemonCache, speciesCache, language, handleSpeciesCache, loadSpecies }) => {
 
     const [searchtext, setSearchtext] = useState('');
-    
+
     const [filteredPokemon, setFilteredPokemon] = useState(pokemonCache);
 
-    const handleSearch = ({target}) => {
+    const handleSearch = ({ target }) => {
         setSearchtext(target.value);
     }
 
     useEffect(() => {
-        if (searchtext && searchtext !== ''){
-            const filtered = pokemonCache.filter(pokemon =>
-                {
-                    return pokemon.name.includes(searchtext.toLowerCase());
-                })
+        if (searchtext && searchtext !== '') {
+            const filtered = pokemonCache.filter(pokemon => {
+                return pokemon.name.includes(searchtext.toLowerCase());
+            })
             setFilteredPokemon(filtered);
         }
         else {
@@ -25,18 +24,20 @@ const Search = ({pokemonCache, speciesCache, language, handleSpeciesCache, loadS
         }
     }, [searchtext, pokemonCache])
 
-    return(
+    return (
         <div className="mh">
             <div className="content">
-            <div className="search-wrapper">
-                <label htmlFor="pokemon-search">Search for pokemons:</label>
-                <input type="search" placeholder="Suche" id="pokemon-search" onChange={handleSearch}/>
-                <ul>
-                    {filteredPokemon.map(pokemon => {
-                        return speciesCache.find(el => el.id === Number(pokemon.id)) ? <SearchResults key={pokemon.id} pokemon={speciesCache.find(el => el.id === Number(pokemon.id))} isSpecies={true} language={language}/> : <SearchResults pokemon={pokemon} key={pokemon.id} isSpecies={false} handleSpeciesCache={handleSpeciesCache} loadSpecies={loadSpecies}></SearchResults>
-                    })}
-                </ul>
-            </div>
+                <div className="search-wrapper">
+                    <label htmlFor="pokemon-search">Search for pokemons:</label>
+                    <input type="search" placeholder="Suche" id="pokemon-search" onChange={handleSearch} />
+                    <div className="result-wrapper">
+                        <ul>
+                            {filteredPokemon.map(pokemon => {
+                                return speciesCache.find(el => el.id === Number(pokemon.id)) ? <SearchResults key={pokemon.id} pokemon={speciesCache.find(el => el.id === Number(pokemon.id))} isSpecies={true} language={language} /> : <SearchResults pokemon={pokemon} key={pokemon.id} isSpecies={false} handleSpeciesCache={handleSpeciesCache} loadSpecies={loadSpecies}></SearchResults>
+                            })}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
